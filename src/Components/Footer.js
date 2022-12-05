3d49d83df924a57031c81d0f001a198481eee4bd
Matheus <matheusvmja@gmail.com>
2022-12-05 13:10:35 -0300

    footer pronto

---
+++ src/Components/Footer.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import seta_play from "../assets/img/seta_play.png";
+
+export default function Footer(props) {
+  const {iconeChange, counter} = props
+  
+  return (
+    <ContainerFooter>
+      <p data-identifier='flashcard-counter'>{counter}/{iconeChange.length} CONCLUÍDOS</p>
+    </ContainerFooter>
+  );
+}
+
+const ContainerFooter = styled.div`
+  width: 100%;
+  min-height: 50px;
+  background-color: #ffffff;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Recursive";
+  font-weight: 400;
+  font-size: 18px;
+  color: #333333;
+  padding: 10px;
+`;
